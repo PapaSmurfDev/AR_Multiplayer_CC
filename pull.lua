4f934075a290ebb133202128331d08e1b03905b4
PapaSmurfDev <96831364+PapaSmurfDev@users.noreply.github.com>
2022-01-04 02:45:26 +0800

    Update pull.lua

--- pull.lua
+++ pull.lua
@@ -3,3 +3,5 @@ local parentDir = fs.getDir(currentDir)
 shell.setDir(parentDir)
 shell.run("github clone PapaSmurfDev/AR_Multiplayer_CC")
 shell.setDir(currentDir)
+alias "PapaSmurfDev/AR_Multiplayer_CC/overlayTest" startAR
+print("Run startAR to start the AR experience!")
